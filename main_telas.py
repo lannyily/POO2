da875254e47ebe2c0cc2f442805ccc88183b85bb
lannyily <erlanny.rego@ufpi.com.br>
2023-11-27 13:23:21 -0300

    atualizei arquivo

--- main_telas.py
+++ main_telas.py
@@ -17,6 +17,7 @@ from tela_perfil import Ui_tela_perfil
 from tela_casa_polvora import Ui_tela_casa_polvora
 from tela_casa_pol_reserva import Ui_tela_casa_pol_reserva
 
+print('oi')
 
 class Ui_Main(QtWidgets.QWidget):
 
